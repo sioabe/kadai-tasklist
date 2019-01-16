40669523fe5dd31aea3a3bce0d2102e90d5728df
sio <m40x3.1wa121@gmail.com>
2019-01-16 12:12:12 +0000

    ルート変更。再々提出

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'toppage',to: 'tasks#index'
-  root to:'users#index'
+  get 'toppage',to: 'users#index'
+  root to:'tasks#index'
   resources :tasks
   
   #ユーザー登録
   get 'signup', to: 'users#new'
-  resources :users, only: [:index, :show, :new, :create]
+  resources :users, only: [:index, :new]
   
   #ログイン機能
   get 'login', to: 'sessions#new'
